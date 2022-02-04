97add439f89302dca6348acefa663ffa9317b266
lessjiu <97648384+lessjiu@users.noreply.github.com>
2022-02-04 12:49:45 -0800

    lib + code that fails

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo {
-
+    public static int subtract(int m, int n) {
+        return n - m;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void subtractTest() {
+        assertEquals(10, SkillDemo.subtract(11-2)); //should fail
+    }
+}
